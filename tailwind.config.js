3c4beee369873028247f0992abfb65b7bf4e5ae6
Mehedi Hasan <devbytemehedi@gmail.com>
2023-08-15 14:04:47 +0600

    🎨 improve structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./404.html", "./pages/**/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
